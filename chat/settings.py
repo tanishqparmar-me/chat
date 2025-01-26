7be9ce89a7202c2e74f29ad3c4bbd90988629dc5
Tanishq_Parmar <128317583+tanishqparmar-me@users.noreply.github.com>
2025-01-26 09:49:06 +0530

    Update settings.py

--- chat/settings.py
+++ chat/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = "chat/chat_app/static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
