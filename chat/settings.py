6eb9dec9bc549685c0d2a9eaabc388d6d572dbf9
Tanishq_Parmar <128317583+tanishqparmar-me@users.noreply.github.com>
2025-01-26 09:54:41 +0530

    Update settings.py

--- chat/settings.py
+++ chat/settings.py
@@ -116,10 +116,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATIC_URL = "home/ubuntu/myproject/chat/static/" # Django will search for /src/
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "home/ubuntu/myproject/chat/static/"), # Django will search for /src/
+]
+STATIC_ROOT = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
